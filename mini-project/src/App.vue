ec70835714a0f8ed737992dab7329dbeb54bdd78
yoonmi <xduathx@naver.com>
2022-06-12 23:15:32 +0900

    미니프로젝트#1

---
+++ mini-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './layouts/Header.vue'
+import Footer from './layouts/Footer.vue'
+import Header from './layouts/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  computed:{
+    user(){
+      return this.$store.state.user;
+    }
+  }
+}
+</script>
---
+++ mini-project/src/layouts/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="mt-5 py-5 bg-dark text-white">
+    &copy; 2022
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
